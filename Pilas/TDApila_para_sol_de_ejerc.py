4ff9989dd4083e56628339cbb4ce9069c7858ae6
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 08:41:10 +0100

    realizando

--- Pilas/TDApila_para_sol_de_ejerc.py
+++ Pilas/TDApila_para_sol_de_ejerc.py
@@ -1,2 +1,21 @@
 from pila import Pila, apilar, desapilar, pila_vacia
 pdatos = Pila()
+ppar = Pila()
+pimpar = Pila()
+dato = int(input("Ingrese un numero - 0 para salir"))
+
+while(dato != 0):
+    apilar(pdatos, dato)
+    dato = int(input("Ingrese un numero - 0 para salir"))
+while(not pila_vacia(pdatos)):
+    dato = desapilar(pdatos)
+    if(dato % 2 == 0):
+        apilar(ppar, dato)
+    else:
+        apilar(pimpar, dato)
+while(not pila_vacia(ppar)):
+    dato = desapilar(ppar)
+    print(dato)
+while(not pila_vacia(pimpar)):
+    dato = desapilar(ppar)
+    print(dato)
