af9c5e0fb86425dafe5e134b558a602ec0dbaa46
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-26 20:10:21 +0200

    colas temrinadas

--- colas/TDACola_para_sol_de_ejerc.py
+++ colas/TDACola_para_sol_de_ejerc.py
@@ -1,1 +1,17 @@
 from colas import colas
+
+cdatos = colas.Cola()
+cvocales = colas.Cola()
+
+letra = input("Ingrese un caracter")
+while(letra != ""):
+    colas.Cola.arribo(cdatos, letra)
+    letra = input("ingrese un caracter")
+while(not colas.Cola.cola_vacia(cdatos)):
+    letra = colas.Cola.atencion(cdatos)
+    if letra.upper() in ["A", "E", "i", "o", "u"]:
+        colas.Cola.arribo(cvocales, letra)
+print("Datos cola vocales")
+while(not colas.Cola.cola_vacia(cvocales)):
+    dato = colas.Cola.atencion(cvocales)
+    print(dato)
