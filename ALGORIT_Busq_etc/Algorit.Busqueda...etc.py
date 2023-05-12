30428b73838038493181fbefcfa38b540fda51f5
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-12 11:05:32 +0200

    Haciendo algoritmos boravces

---
+++ ALGORIT_Busq_etc/Algorit.Busqueda...etc.py
@@ -0,0 +1,62 @@
+from Herramientas_necesarias import Heap, Pila, Arista, inf
+def dijkstra(grafo, origen, destino):
+    no_visitados = Heap(Pila.Tamaño(grafo))
+    camino = Pila()
+    aux = grafo.inicio
+    while(aux is not None):
+        if(aux.info == origen):
+            Heap.arrib_H(no_visitados, [aux, None], 0)
+        else:
+            Heap.arrib_H(no_visitados, [aux, None], inf)
+        aux = aux.sig
+    while(not Heap.heap_vacio(no_visitados)):
+        dato = Heap.atencion_H(no_visitados)
+        Pila.apilar(camino, dato)
+        aux = dato[1][0].adyacentes.inicio
+        while(aux is not None):
+            pos = buscar_H(no_visitados, aux.destino)
+            if(no_visitados.vector[pos][0]>dato[0] + aux.info):
+                no_visitados.vector[pos][1][1] = dato[1][0].info
+                Heap.cambiar_prioridad(no_visitados, pos, dato[0] + aux.info)
+            aux = aux.sig
+    return camino
+
+def prim(grafo):
+    bosque = [[grafo.inicio.info]]
+    aristas = []
+    adyacentes = grafo.inicio.adyacentes.inicio
+    while(adyacentes is not None):
+        aristas.append([grafo.inicio.info, adyacentes.destino, adyacentes.info])
+        adyacentes = adyacentes.sig
+    while(len(bosque[0]) // 2 < Pila.Tamaño(grafo) - 1):
+        menor = inf
+        menor_arista = None
+        tipo = None
+        for arista in aristas:
+            origen = arista[0]
+            destino = arista[1]
+            if(origen not in bosque[0] and destino in bosque[0]):
+                if(arista[2] < menor):
+                    menor, menor_arista = arista[2], arista
+                    tipo = True
+            if(origen in bosque[0] and destino not in bosque[0]):
+                if(arista[2] < menor):
+                    menor, menor_arista = arista[2], arista
+                    tipo = False
+
+        arista = aristas.pop(aristas.index(menor_arista))
+        if(len(bosque[0] != 1)):
+            bosque[0] += [arista[0], arista[1]]
+        else:
+            bosque.pop()
+            bosque.append([arista[0], arista[1]])
+        aux = None
+        if(tipo):
+            aux = Arista.buscar_vertice(grafo, arista[0])
+        else:
+            aux = Arista.buscar_vertice(grafo, arista[1])
+        adyacentes = aux.adyacentes.inicio
+        while(adyacentes is not None):
+            aristas.append([aux.info, adyacentes.destino, adyacentes.info])
+            adyacentes = adyacentes.sig
+    return bosque
--- ALGORIT_Busq_etc/Herramientas_necesarias.py
+++ ALGORIT_Busq_etc/Herramientas_necesarias.py
@@ -1,3 +1,19 @@
+# PILAS
+class nodoPila(object):
+    info, sig = None, None
+class Pila(object):
+    def __init__(self):
+        self.cima = None
+        self.tamaño = 0
+    def apilar(pila, dato):
+        nodo = nodoPila()
+        nodo.info = dato
+        nodo.sig = pila.cima
+        pila.tamaño += 1
+    def Tamaño(pila):
+        return pila.tamaño
+
+# MONTICULO
 class Heap(object):
     def __init__(self, tamaño):
         self.tamaño = 0
@@ -6,6 +22,8 @@ class Heap(object):
         aux = vector[indice1]
         vector[indice1] = vector[indice2]
         vector[indice2] = aux
+    def heap_vacio(heap):
+        return heap.tamaño == 0
     def flotar(heap, indice):
         while(indice > 0 and heap.vector[indice] > heap.vector[(indice - 1) // 2]):
             padre = (indice - 1) // 2
@@ -26,6 +44,7 @@ class Heap(object):
                 hijo_izq = (indice * 2) + 1
             else:
                 control = False
+
     def agregar(heap, dato):
         heap.vector[heap.tamaño] = dato
         Heap.flotar(heap, heap.tamaño)
@@ -36,20 +55,11 @@ class Heap(object):
         heap.tamaño -= 1
         Heap.hundir(heap, 0)
         return dato
-    def cantidad_elementos(heap):
-        return heap.tamaño
-    def heap_vacio(heap):
-        return heap.tamaño == 0
-    def heap_lleno(heap):
-        return heap.tamaño == len(heap.vector)
-    def monticulizar(heap):
-        for i in range(len(heap.vector)):
-            Heap.flotar(heap, i)
-    # MONTICULO COMO COLA DE PRIORIDAD
     def arrib_H(heap, dato, prioridad):
         Heap.agregar(heap, [prioridad, dato])
     def atencion_H(heap):
         return Heap.Quitar(heap)[1]
+
     def cambiar_prioridad(heap, indice, prioridad):
         prioridad_anterior = heap[indice][0]
         heap[indice][0] = prioridad
@@ -57,3 +67,14 @@ class Heap(object):
             Heap.flotar(heap, indice)
         elif(prioridad < prioridad_anterior):
             Heap.hundir(heap, indice)
+
+# GRAFOS
+class Arista(object):
+    def __init__(self):
+        self.inicio = None
+        self.tamaño = 0
+    def buscar_vertice(grafo, buscado):
+        aux = grafo.inicio
+        while(aux is not None and aux.info != buscado):
+            aux = aux.sig
+        return aux
