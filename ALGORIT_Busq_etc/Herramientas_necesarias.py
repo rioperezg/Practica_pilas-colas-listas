4628e599a2d684f69824098861fc416b22e6fa77
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-12 11:49:41 +0200

    teminados tdas

--- ALGORIT_Busq_etc/Herramientas_necesarias.py
+++ ALGORIT_Busq_etc/Herramientas_necesarias.py
@@ -10,6 +10,13 @@ class Pila(object):
         nodo.info = dato
         nodo.sig = pila.cima
         pila.tamaño += 1
+    def desapilar(pila):
+        x = pila.cima.info
+        pila.cima = pila.cima.sig
+        pila.tamaño -= 1
+        return x
+    def pila_vacia(pila):
+        return pila.cima is None
     def Tamaño(pila):
         return pila.tamaño
 
@@ -74,7 +81,38 @@ class Heap(object):
         elif(prioridad < prioridad_anterior):
             Heap.hundir(heap, indice)
 
+# COLAS
+class nodoCola(object):
+    info, sig = None, None
+class Cola(object):
+    def __init__(self):
+        self.frente, self.final = None, None
+        self.tamaño = 0
+    def arribo(cola, dato):
+        nodo = nodoCola()
+        nodo.info = dato
+        if cola.frente is None:
+            cola.frente = nodo
+        else:
+            cola.final.sig = nodo
+        cola.final = nodo
+        cola.tamaño += 1
+    def atencion(cola, dato):
+        dato = cola.frente.info
+        cola.frente = cola.frente.sig
+        if cola.frente is None:
+            cola.final = None
+            cola.tamaño -= 1
+            return dato
+    def cola_vacia(cola):
+        return cola.frente is None
+
 # GRAFOS
+class Grafo(object):
+    def __init__(self, dirigido = True):
+        self.inicio = None
+        self.dirigido = dirigido
+        self.tamaño = 0
 class Arista(object):
     def __init__(self):
         self.inicio = None
@@ -84,3 +122,51 @@ class Arista(object):
         while(aux is not None and aux.info != buscado):
             aux = aux.sig
         return aux
+    def barrido_profundo(grafo, vertice):
+        while(vertice is not None):
+            if(not vertice.visitado):
+                vertice.visitado = True
+                print(vertice.info)
+                adyacentes = vertice.adyacentes.inicio
+                while(adyacentes is not None):
+                    adyacente = Arista.buscar_vertice(grafo, adyacentes.destino)
+                    if(not adyacente.visitado):
+                        Arista.barrido_profundo(grafo, adyacente)
+                    adyacentes = adyacentes.sig
+            vertice = vertice.sig
+    def barrido_amplitud(grafo, vertice):
+        cola = Cola()
+        while(vertice is not None):
+            if(not vertice.visitado):
+                vertice.visitado = True
+                Cola.arribo(cola, vertice)
+                while(not Cola.cola_vacia(cola)):
+                    nodo = Cola.atencion(cola)
+                    print(nodo.info)
+                    adyacentes = nodo.adyacentes.inicio
+                    while(adyacentes is not None):
+                        adyacente = Arista.buscar_vertice(grafo, adyacentes.destino)
+                        if(not adyacente.visitado):
+                            adyacente.visitado = True
+                            Cola.arribo(cola, adyacente)
+                        adyacentes = adyacentes.sig
+            vertice = vertice.sig
+
+    def existe_paso(grafo, origen, destino):
+        resultado = False
+        if(not origen.visitado):
+            origen.visitado = True
+            vadyacentes = origen.adyacentes.inicio
+            while(vadyacentes is not None and not resultado):
+                adyacente = Grafo.buscar_vertice(grafo, vadyacentes.destino)
+                if(adyacente.info == destino.info):
+                    resultado = True
+                elif(not adyacente.visitado):
+                    resultado = Grafo.existe_paso(grafo, adyacente, destino)
+                vadyacentes = vadyacentes.sig
+        return resultado
+    def marcar_no_visitado(grafo):
+        aux = grafo.inicio
+        while(aux is not None):
+            aux.visitado = False
+            aux = aux.sig
--- ALGORIT_Busq_etc/TDAAlgor_para_sol_de_ejerc.py
+++ ALGORIT_Busq_etc/TDAAlgor_para_sol_de_ejerc.py
@@ -1,1 +1,23 @@
-from 
+from algorit_Busqueda_etc import dijkstra, Kruskal
+from Herramientas_necesarias import Pila, Cola, Heap, Grafo, Arista
+grafo = Grafo(False)
+cargar_grafo(grafo)
+Arista.barrido_profundo(grafo, grafo.inicio)
+Arista.marcar_no_visitado(grafo)
+Arista.barrido_amplitud(grafo, grafo.inicio)
+
+origen = Arista.buscar_vertice(grafo, 'T')
+destino = Arista.buscar_vertice(grafo, 'Z')
+camino_mas_corto = None
+if(origen is not None and destino is not None):
+    if(Arista.existe_paso(grafo, origen, destino)):
+        camino_mas_corto = dijkstra(grafo, "T", "Z")
+        fin = destino.info
+        while(not Pila.pila_vacia(camino_mas_corto)):
+            dato = Pila.desapilar(camino_mas_corto)
+            if(fin == dato[1][0].info):
+                print(dato[1][0].info)
+                fin = dato[1][1]
+Arista.marcar_no_visitado(grafo)
+arbol_minimo = Kruskal(grafo)
+print(arbol_minimo)
