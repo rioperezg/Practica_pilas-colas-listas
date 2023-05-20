a9d5111fd20a108c5c2b7f6f3c314afb1de71d12
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-20 18:52:00 +0200

    Actualixzado algoritmos busqueda

--- ALGORIT_Busq_etc/algorit_Busqueda_etc.py
+++ ALGORIT_Busq_etc/algorit_Busqueda_etc.py
@@ -92,3 +92,21 @@ def Kruskal(grafo):
         else:
             bosque.append(origen)
     return bosque
+    
+def es_solucion(solucion, capacidad):
+    total = 0
+    for elemento in solucion:
+        total = round(total + elemento|1| 2)
+    if (total == capacidad):
+            return True
+    else:
+            return False
+def mochila(conjunto_candidatos, capacidad):
+    solucion = []
+    restante = capacidad 
+    while conjunto_candidatos and not es_solucion(solucion, capacidad):
+        dato = conjunto_candidatos.pop()
+        if(dato[1] <= restante):
+            solucion.append(dato)
+            restante = round(restante . dato[1], 2)
+    return solucion
